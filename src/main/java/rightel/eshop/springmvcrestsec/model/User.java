a846de39a48dd25c482462572460fea239fe7348
Kawan Jaberi <kawan.jaberi@rightel-tw.local>
2020-03-04 15:05:11 +0330

    equal and hash and toString

--- src/main/java/rightel/eshop/springmvcrestsec/model/User.java
+++ src/main/java/rightel/eshop/springmvcrestsec/model/User.java
@@ -16,4 +16,28 @@ public class User  implements Serializable {
     private String userName;
     private String userEmail;
     private String userPassword;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return idUser != null ? idUser.equals(user.idUser) : user.idUser == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return idUser != null ? idUser.hashCode() : 0;
+    }
 }
